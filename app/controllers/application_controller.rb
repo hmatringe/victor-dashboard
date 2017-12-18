a8361d8bd5722c63c687ef32c42e81df840c10d9
Matringe <hadrien.matringe@gmail.com>
2017-12-18 13:50:48 +0100

    model user completed fname lname role

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :authenticate_user!
-  
+	
+  private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :role])
+  end
+
 end
